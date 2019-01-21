a5905c9d78b753b46bbcdeeff3d102491019d6d2
ogunb <babacanogun@gmail.com>
2019-01-22 02:27:20 +0300

    change apps language to english.

--- src/components/AddWorkout/WeightsForm.tsx
+++ src/components/AddWorkout/WeightsForm.tsx
@@ -10,7 +10,7 @@ class WeightsForm extends Component {
 				<input
 					type="number"
 					className="form-control form-control-lg"
-					placeholder="Lifted Weight"
+					placeholder="...kg"
 					required
 				/>
 			</div>
