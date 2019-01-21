da0f8130143d468c93bfb0b94d2ae288b11a4515
ogunb <babacanogun@gmail.com>
2019-01-22 01:31:09 +0300

    change apps language to english.

--- src/utils.ts
+++ src/utils.ts
@@ -6,5 +6,5 @@ export function dateToDisplay(newDate: string): string {
 		weekday: 'long'
 	};
 	const date = new Date(newDate);
-	return date.toLocaleDateString('tr-TR', options);
+	return date.toLocaleDateString('en-GB', options);
 }
